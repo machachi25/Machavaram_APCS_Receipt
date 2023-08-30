dfb20eedfdaa14e46f5a6dc9f618eacdc33c6984
Chinmay <machachi25@7hills.org>
2023-08-30 13:03:07 -0400

    cheese

---
+++ Receipt.java
@@ -0,0 +1,11 @@
+package Machavaram_APCS_Receipt;
+
+import java.util.Scanner;
+
+public class Receipt
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
