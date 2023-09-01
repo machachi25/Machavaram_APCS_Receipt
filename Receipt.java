619cdbe89d1f41bc44b0e2ca1d27942a477d18a0
Chinmay <machachi25@7hills.org>
2023-09-01 12:44:21 -0400

    Added another user input with ints

--- Receipt.java
+++ Receipt.java
@@ -13,6 +13,8 @@ public class Receipt
        System.out.println("Welcome to Chin-pie's cookie store.");
        System.out.println("Which one would you like?");
        Cookie = order.nextLine();
+       System.out.println("How many " + Cookie + " do you want?");
+       numCookie = order.nextInt();
 
 
     }
