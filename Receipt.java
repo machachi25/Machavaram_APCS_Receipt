04133a61944eb821f61d9977fbc35b73f9b44d4c
Chinmay <machachi25@7hills.org>
2023-09-06 12:12:34 -0400

    Adding menu as this is being sent

--- Receipt.java
+++ Receipt.java
@@ -11,10 +11,16 @@ public class Receipt
        double priceCookie;
        Scanner order = new Scanner(System.in);
        System.out.println("Welcome to Chin-pie's cookie store.");
+       System.out.println("Each individual cookie is 10 dollars.");
+       System.out.println("We currently have the following: ");
+       System.out.println("Chocolate chip");
        System.out.println("Which one would you like?");
        Cookie = order.nextLine();
        System.out.println("How many " + Cookie + " do you want?");
        numCookie = order.nextInt();
+       priceCookie = numCookie * 10.00;
+       System.out.println("The total amount your paying is $" + priceCookie + ". Have a good day!");
+       priceCookie = order.nextDouble();
 
 
     }
